50fe5e724fe023fe247f474e834a561fa82b9442
Arturas Aleksandraus <arturas@aleksandraus.se>
2025-04-08 15:37:39 +0200

    add med datasets

--- datasets.py
+++ datasets.py
@@ -1,14 +1,10 @@
 import torch
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+from medmnist import INFO
+import medmnist
 
-def get_label(dataset):
-    if isinstance(dataset, (datasets.MNIST, datasets.FashionMNIST, datasets.CIFAR10, datasets.CIFAR100)):
-        return torch.tensor(dataset.targets)
-    elif isinstance(dataset, datasets.SVHN):
-        return torch.tensor(dataset.labels)
-    else:
-        raise ValueError(f'Invalid dataset: {dataset}')
+
 class OneHotEncoding:
     def __init__(self, num_classes):
         self.num_classes = num_classes
@@ -35,36 +31,58 @@ class CoolDataset():
 
     def __len__(self):
         return len(self.dataset)
-    
-def get_dataset(dataset, data_path, transform=None):
+ALL_DATASETS = ['MNIST'
+                ,'FashionMNIST'
+                ,'SVHN'
+                ,'CIFAR10'
+                ,'CIFAR100'
+                ,'PathMNIST'
+                ,'ChestMNIST'
+                ,'DermaMNIST'
+                ,'OCTMNIST'
+                ,'BloodMNIST'
+                ,'BreastMNIST'
+                ,'PneumoniaMNIST'
+                ,'RetinaMNIST'
+                ,'TissueMNIST'
+                ,'OrganAMNIST'
+                ,'OrganCMNIST'
+                ,'OrganSMNIST'
+                ]
+def get_dataset(dataset, data_path, **kvargs):
     if dataset == 'MNIST':
         channel = 1
         im_size = 28
         num_classes = 10
-        ds_train = datasets.MNIST(data_path, train=True, download=True, transform=transform) # no augmentation
-        ds_test = datasets.MNIST(data_path, train=False, download=True, transform=transform)
+        ds_train = datasets.MNIST(data_path, train=True, download=True) # no augmentation
+        ds_test = datasets.MNIST(data_path, train=False, download=True)
         class_names = [str(c) for c in range(num_classes)]
+        transform = lambda x: transforms.functional.to_tensor(x) * 2 -1
+        label_transform = OneHotEncoding(num_classes)
     elif dataset == 'FashionMNIST':
         channel = 1
         im_size = 28
         num_classes = 10
-        ds_train = datasets.FashionMNIST(data_path, train=True, download=True, transform=transform) # no augmentation
-        ds_test = datasets.FashionMNIST(data_path, train=False, download=True, transform=transform)
+        ds_train = datasets.FashionMNIST(data_path, train=True, download=True) # no augmentation
+        ds_test = datasets.FashionMNIST(data_path, train=False, download=True)
         class_names = ds_train.classes
+        transform = lambda x: transforms.functional.to_tensor(x) * 2 -1
+        label_transform = OneHotEncoding(num_classes)
     elif dataset == 'SVHN':
         channel = 3
         im_size = 32
         num_classes = 10
-        ds_train = datasets.SVHN(data_path, split='train', download=True, transform=transform)  # no augmentation
-        ds_test = datasets.SVHN(data_path, split='test', download=True, transform=transform)
+        ds_train = datasets.SVHN(data_path, split='train', download=True)  # no augmentation
+        ds_test = datasets.SVHN(data_path, split='test', download=True)
         class_names = [str(c) for c in range(num_classes)]
-
+        transform = lambda x: transforms.functional.to_tensor(x) * 2 -1
+        label_transform = OneHotEncoding(num_classes)
     elif dataset == 'CIFAR10':
         channel = 3
         im_size = 32
         num_classes = 10
-        ds_train = datasets.CIFAR10(data_path, train=True, download=True, transform=transform) # no augmentation
-        ds_test = datasets.CIFAR10(data_path, train=False, download=True, transform=transform)
+        ds_train = datasets.CIFAR10(data_path, train=True, download=True) # no augmentation
+        ds_test = datasets.CIFAR10(data_path, train=False, download=True)
         class_names = ds_train.classes
         transform = lambda x: transforms.functional.to_tensor(x) * 2 -1
         label_transform = OneHotEncoding(num_classes)
@@ -72,9 +90,34 @@ def get_dataset(dataset, data_path, transform=None):
         channel = 3
         im_size = 32
         num_classes = 100
-        ds_train = datasets.CIFAR100(data_path, train=True, download=True, transform=transform) # no augmentation
-        ds_test = datasets.CIFAR100(data_path, train=False, download=True, transform=transform)
+        ds_train = datasets.CIFAR100(data_path, train=True, download=True) # no augmentation
+        ds_test = datasets.CIFAR100(data_path, train=False, download=True)
         class_names = ds_train.classes
+        transform = lambda x: transforms.functional.to_tensor(x) * 2 -1
+        label_transform = OneHotEncoding(num_classes)
+    elif dataset in ['PathMNIST'
+                    ,'ChestMNIST'
+                    ,'DermaMNIST'
+                    ,'OCTMNIST'
+                    ,'BloodMNIST'
+                    ,'BreastMNIST'
+                    ,'PneumoniaMNIST'
+                    ,'RetinaMNIST'
+                    ,'TissueMNIST'
+                    ,'OrganAMNIST'
+                    ,'OrganCMNIST'
+                    ,'OrganSMNIST'
+                    ]:
+        info = INFO[dataset.lower()]    
+        channel = info['n_channels']
+        im_size = kvargs.get("size", 28)
+        num_classes = len(info["label"])
+        DataClass = getattr(medmnist, info['python_class'])
+        ds_train = DataClass(split='train',download=True, size=im_size, root=data_path)
+        ds_test = DataClass(split='test',download=True, size=im_size, root=data_path)
+        class_names = list(info["label"].values())
+        transform = lambda x: (transforms.functional.to_tensor(x)  - 0.5) / 0.5
+        label_transform = OneHotEncoding(num_classes)
     else:
         raise ValueError(f'Invalid dataset: {dataset}')
     ds_test = CoolDataset(ds_test, im_size, num_classes, channel, transform, label_transform)
@@ -85,7 +128,5 @@ def get_dataset(dataset, data_path, transform=None):
         'num_classes': num_classes,
         'train': ds_train,
         'test': ds_test,
-        'label_train': get_label(ds_train.dataset),
-        'label_test': get_label(ds_test.dataset),
         'class_names': class_names
     }
